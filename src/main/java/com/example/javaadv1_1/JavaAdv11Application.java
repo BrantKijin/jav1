7a1bdc9167ee1d9f109e268d708adbc64d5dda97
곽기진 <upper0house@gmail.com>
2025-02-07 20:17:29 +0900

    1

---
+++ src/main/java/com/example/javaadv1_1/JavaAdv11Application.java
@@ -0,0 +1,13 @@
+package com.example.javaadv1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdv11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdv11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaadv1_1/JavaAdv11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaadv1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdv11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
